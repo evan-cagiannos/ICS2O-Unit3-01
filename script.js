9cef2bc716be1e4020462751292a56ee0b719596
evan-cagiannos <99355231+evan-cagiannos@users.noreply.github.com>
2022-03-30 17:18:15 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Created by Evan Cagiannos
+// ICS2O-UNIT2-02-HTML
+// Created on: 2022-March-10 
+//This file contains the JS functions for index.html
+
+/**
+* Does basic math
+*/
+
+function doMathClicked() {
+  // this function does basic math
+  // input
+  const sidea = parseInt(document.getElementById('sidea-of-trapezoid').value)
+  const base = parseInt(document.getElementById('base-of-trapezoid').value)
+  const sidec = parseInt(document.getElementById('sidec-of-trapezoid').value)
+  const sided = parseInt(document.getElementById('sided-of-trapezoid').value)
+  const height = parseInt(document.getElementById('height-of-trapezoid').value)
